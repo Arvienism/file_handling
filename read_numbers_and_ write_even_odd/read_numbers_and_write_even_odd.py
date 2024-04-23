d0294b0f6fea48826462a57927758df452e933aa
Arvie Lastra <lastraarvie@gmail.com>
2024-04-24 02:14:52 +0800

    Updated text file to integer

--- read_numbers_and_ write_even_odd/read_numbers_and_write_even_odd.py
+++ read_numbers_and_ write_even_odd/read_numbers_and_write_even_odd.py
@@ -1,12 +1,16 @@
 # pseudocode
 
 # start:
+even_numbers = []
+odd_numbers = []
+
 #   read the text file that contains 20 integer
 with open("number.txt", "r") as f:
     numbers = f.readlines()
 #   convert text file to integer
+numbers = []
 for num in numbers:
-    numbers = [int(num.strip())]
+    numbers = int(num.strip())
 #   separate the number to even and odd
 #   write the even numbers to even.txt
 #   write the odd numbers to odd.txt
