f8f9fdcff8764d628e9e9eb3cb81da76adea1041
Arvie Lastra <lastraarvie@gmail.com>
2024-04-24 05:46:19 +0800

    Added loop for the info on the text file

--- read_students_gwa/read_students_gwa.py
+++ read_students_gwa/read_students_gwa.py
@@ -7,7 +7,8 @@ with open("read_students_gwa/students_gwa.txt", "r") as f:
 highest_student= ""
 max_gwa = 1
 #   create a loop to find the highest gwa and name
-#       split the name and gwa from text file
-#       convert string to float
+for info in students:
+    name, gwa = info.split() #split the name and gwa from text file
+    gwa = float(gwa) #convert string to float
 #       check if the student's gwa is the highest
 #   print the student with highest gwa in a "MAANGAS WAY" or "wow factor effect"
