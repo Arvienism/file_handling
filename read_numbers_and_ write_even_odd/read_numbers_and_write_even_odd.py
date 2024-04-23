500a0e7c873b8c08ec17f258a41bfed60768fc07
Arvie Lastra <lastraarvie@gmail.com>
2024-04-24 03:24:06 +0800

    Updated separate even and odd

--- read_numbers_and_ write_even_odd/read_numbers_and_write_even_odd.py
+++ read_numbers_and_ write_even_odd/read_numbers_and_write_even_odd.py
@@ -6,11 +6,10 @@ odd_numbers = []
 
 #   read the text file that contains 20 integer
 with open("number.txt", "r") as f:
-    numbers = f.readlines()
+    number = f.readlines()
 #   convert text file to integer
-numbers = []
-for num in numbers:
-    numbers = int(num.strip())
+for num in number:
+    num = int(num.strip())
 #   separate the number to even and odd
     if num % 2 == 0:
         even_numbers.append(num)
