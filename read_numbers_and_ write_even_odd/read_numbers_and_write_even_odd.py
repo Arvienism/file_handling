c160e99f86508fdac706b0f75f460d5e8f470912
Arvie Lastra <lastraarvie@gmail.com>
2024-04-24 00:04:17 +0800

    Added pseudocode

---
+++ read_numbers_and_ write_even_odd/read_numbers_and_write_even_odd.py
@@ -0,0 +1,10 @@
+# pseudocode
+
+# start:
+#   create text file that contains 20 integer
+#   read the text file that contains 20 integer
+#   convert text file to integer
+#   separate the number to even and odd
+#   write the even numbers to even.txt
+#   write the odd numbers to odd.txt
+# end:
