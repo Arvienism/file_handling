e6b06280ad9b891972fd38212f2f6d2ea61d6a47
Arvie Lastra <lastraarvie@gmail.com>
2024-04-25 17:19:13 +0800

    Added read the text file

--- read_integers_and_write_the_double_triple_of_integers/read_integers_and_write_double_triple_of_integers.py
+++ read_integers_and_write_the_double_triple_of_integers/read_integers_and_write_double_triple_of_integers.py
@@ -1,6 +1,8 @@
 # start:
 #   create text file named integers.txt that contains 20 integers
 #   read the integers.txt
+with open("integers.txt", "r") as integers_file:
+    number = integers_file.readlines()
 #   convert the integers.txt to integer
 #   separate the integers to even and odd
 #   square all even integers
