28d3d8463a103bc084a7c4f318f5c056d2454675
Arvie Lastra <lastraarvie@gmail.com>
2024-04-25 16:59:33 +0800

    Added pseudocode

---
+++ read_integers_and_write_the_double_triple_of_integers/read_integers_and_write_double_triple_of_integers.py
@@ -0,0 +1,10 @@
+# start:
+#   create text file named integers.txt that contains 20 integers
+#   read the integers.txt
+#   convert the integers.txt to integer
+#   separate the integers to even and odd
+#   square all even integers
+#   cube all the odd integers
+#   write all squared integers to double.txt
+#   write all cubed integers to triple.txt
+# end:
