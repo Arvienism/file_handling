82ec34e9ec9acb39f313cd0eaef94a423ff928f1
Arvie Lastra <lastraarvie@gmail.com>
2024-04-25 13:04:15 +0800

    Added pseudocode

--- write_multiple_line_of_text/write_multiple_line_of_text.py
+++ write_multiple_line_of_text/write_multiple_line_of_text.py
@@ -1,4 +1,16 @@
 # pseudocode
 #
 # start:
-#   
+#   prompts user to input a line
+#   create a loop to write a line
+#       ask user if they want to creat more lines
+#       prompts user to input no to stop the loop 
+#       write the inputted line to the mylife.txt
+#       create an infinite loop to asks user for new multiple lines
+#           prompts the user to input a  new line
+#           write the inputted new line to the mylife.txt
+#           asks user if they want to create more lines
+#           create a loop to know if the user wants to stop
+#               prompts user to input no to stop the loop
+#       break the infinite loop
+# end:
